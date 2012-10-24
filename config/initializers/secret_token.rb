c1ea5cec6086b30b757c643f4b0d976c3c6ca2f0
CalvinFernandez <cfernand@cs.stanford.edu>
2012-10-24 15:05:25 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinchapp::Application.config.secret_token = '294a2c7f250147b2bbe8a99a87ea5872113436591b2fb53f53aba7da8cb42387edf0ef46de231db5496233fac186c84a98dbba376fc94bf3f7cd7667ad3634ce'
