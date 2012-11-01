d84403702aa5fcc5e2ab6d57d666815139a57170
CalvinFernandez <cfernand@cs.stanford.edu>
2012-11-01 12:57:51 -0700

    can get alerts, now trying to test pinch distance

--- public/myLogic.js
+++ public/myLogic.js
@@ -166,7 +166,7 @@ $(function(){
 
         container.bind("transform", function(event) {
             e = event
-            alert("I can get alerts");
+            alert(Math.abs(e.touches[0].x - e.touches[1].x));
             if ( Math.abs(e.touches[0].x - e.touches[1].x) < 2 )
             {
               if ( Math.abs( e.touches[0].y - e.touches[1].y) < 2 )
