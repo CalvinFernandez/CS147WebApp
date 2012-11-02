439761b828909b0b365401dc90d8cc6b3d43d686
CalvinFernandez <cfernand@cs.stanford.edu>
2012-11-01 17:51:04 -0700

    attempting to add image placement for get

--- public/myLogic.js
+++ public/myLogic.js
@@ -283,7 +283,10 @@ $(function(){
                 document.
                 document.getElementById("listener")
                 .append("<div id='getZoom' class='zoomProps'><div class='polaroid'><img src='images/screen.jpg' alt='' width='200' height='200' /><span> My pic </span></div></div>");
-
+                if (document.getElementById("getZoom") != null)
+                {
+                  alert("It exists somewhere");
+                }
                 //Initialize all listeners on this object so it can be moved around//
                 var getZoom = new ZoomView('#getZoom', '#getZoom :first');
 
